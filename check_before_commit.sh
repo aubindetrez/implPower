b038f9a45656830fc856e9840eab1ad63f415487
Aubin Detrez <aubin.detrez@gmail.com>
2022-07-12 21:45:16 +0200

    Run auto-formatting before checking syntax with the Linter

--- check_before_commit.sh
+++ check_before_commit.sh
@@ -1,23 +1,6 @@
 gitroot="`git rev-parse --show-toplevel`"
 cd $gitroot
 
-# Run Linter on all SystemVerilog files
-if ! command -v verible-verilog-lint &> /dev/null
-then
-    echo "Error: verible-verilog-lint is not installed on your system"
-    echo "You can get it from: https://github.com/chipsalliance/verible"
-    echo "Make sure it is in your PATH before trying again"
-    exit 1
-fi
-for file in `find ./Logic/ -name "*.sv"`
-do echo ">>> Running Linter on $file"
-    verible-verilog-lint $file
-    if [[ $? -ne 0 ]]
-    then echo "==> Check failed, please fix it and try again"
-        exit 1
-    fi
-done
-
 # Run formatter on all SystemVerilog files to ensure a consistence reading
 # experience to all contributors
 if ! command -v verible-verilog-format &> /dev/null
@@ -36,6 +19,22 @@ do echo ">>> Running formatter on $file"
     fi
 done
 
+# Run Linter on all SystemVerilog files
+if ! command -v verible-verilog-lint &> /dev/null
+then
+    echo "Error: verible-verilog-lint is not installed on your system"
+    echo "You can get it from: https://github.com/chipsalliance/verible"
+    echo "Make sure it is in your PATH before trying again"
+    exit 1
+fi
+for file in `find ./Logic/ -name "*.sv"`
+do echo ">>> Running Linter on $file"
+    verible-verilog-lint $file
+    if [[ $? -ne 0 ]]
+    then echo "==> Check failed, please fix it and try again"
+        exit 1
+    fi
+done
 
 # Run all Functional tests by calling "make" in every directories in FuncVerif/
 for dir in FuncVerif/*
