a8f76e99a59d81f2e845a2148859fc16d7c977f5
Aubin Detrez <aubin.detrez@gmail.com>
2022-07-14 19:15:02 +0200

    Identity Unit: Update test.sh
    Update the test script to check if cocotb's report exists before
    parsing it.
    Otherwise if the simulation crashed the script would report a PASS

--- FuncVerif/Identify/test.sh
+++ FuncVerif/Identify/test.sh
@@ -1,5 +1,13 @@
 echo ">>> Running Verification (Actual Logic simulation)"
 make
+
+# Check if a result if available
+if ! [ -f $result_file ]
+then
+    echo ">>> Error: Verification failed"
+    exit 1
+fi
+
 # Counts how many line contains a failure
 fails=`grep "failure" results.xml | wc -l`
 if ! [ $fails -eq 0 ]
